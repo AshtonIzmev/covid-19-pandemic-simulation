a9c66507c4d552261a2be0c63afb9e7fddccaf5c
Issam <biossamu@gmail.com>
2021-03-17 21:52:37 +0100

    New version release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pandemic-simulation',
-    version='0.1.3',
+    version='0.2.0',
     description='"Life simulation" of a SEIR inspired model to better understand pandemic using python',
     long_description_content_type="text/plain",
     long_description="Check https://github.com/AshtonIzmev/covid-19-pandemic-simulation/blob/master/README.md",
